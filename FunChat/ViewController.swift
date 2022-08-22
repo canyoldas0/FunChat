4ebd7f08d1886da26b2bb03c6859c6d75dae1972
Can Yoldas <can.yoldas0@outlook.com>
2022-08-22 18:15:28 +0300

    Initial Commit

---
+++ FunChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FunChat
+//
+//  Created by Can Yoldaş on 22.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
